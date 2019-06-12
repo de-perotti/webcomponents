4251f98520f3e34ca4cb97bbbab2784eef29c617
Luigi Perotti <luigi.perotti@quero.com>
2019-06-12 19:16:30 -0300

    Vai pokemon

--- src/counter/Counter.tsx
+++ src/counter/Counter.tsx
@@ -1,39 +1,36 @@
 /* eslint-disable react/no-unknown-property */
 import React from 'react';
+import './counter.scss';
 
 class ActualCounter extends React.Component {
   state = { count: 0 };
 
   render() {
     return (
-      <div>
+      <div className="counter">
         {this.state.count}
-        {React.createElement(
-          'button',
-          {
-            type: 'button',
-            onClick: () => {
-              console.log('adding up');
-              this.setState((state: typeof ActualCounter.prototype.state) => ({
-                count: state.count + 1,
-              }));
-            },
-          },
-          '+',
-        )}
-        {React.createElement(
-          'button',
-          {
-            type: 'button',
-            onClick: () => {
-              console.log('subtracting up');
-              this.setState((state: typeof ActualCounter.prototype.state) => ({
-                count: state.count - 1,
-              }));
-            },
-          },
-          '-',
-        )}
+        <button
+          type="button"
+          onClick={() => {
+            console.log('adding up');
+            this.setState((state: typeof ActualCounter.prototype.state) => ({
+              count: state.count + 1,
+            }));
+          }}
+        >
+          +
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            console.log('subtracting');
+            this.setState((state: typeof ActualCounter.prototype.state) => ({
+              count: state.count - 1,
+            }));
+          }}
+        >
+          -
+        </button>
       </div>
     );
   }
